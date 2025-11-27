95ec9c2b5afcd2ef30f0cf45926cf2a32a399397
Balamurugan <balamuruganelangovan.official@gmail.com>
2025-11-27 23:12:49 +0530

    Functions types

--- Function.py
+++ Function.py
@@ -1,4 +1,46 @@
 def add_pannu(a,b):
     result=a+b
     return result
-add_pannu(5,10)
+output=add_pannu(5,10)
+print(output)
+
+new=add_pannu(4,5)
+print(new)
+
+#Build in function
+
+a=2
+b=3
+c=pow(a,b)
+print(c)
+
+e=10
+a=[1,2,3,4,5]
+print(min(a))
+print(max(a))
+
+
+#user defined function
+
+def call_pannu(a):
+    result=a
+    return result
+
+print(call_pannu("call Balamurugan"))
+
+#default argumrt
+
+def say_hello():
+    print("Hello")
+
+def total(*numbers):
+    print("Total=",sum(numbers))
+total(10,20,30)
+
+
+#lamda function
+
+add_pannu=lambda a,b :a*b
+
+output=(add_pannu(10,5))
+print(output)
