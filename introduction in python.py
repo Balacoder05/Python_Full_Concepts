e9291adaad335b30212746de6821928b1c7dfaf8
Balamurugan <balamuruganelangovan.official@gmail.com>
2025-11-27 11:37:50 +0530

    Print stament

--- introduction in python.py
+++ introduction in python.py
@@ -10,3 +10,25 @@ b="Python programming"
 print(a)
 print(b)
 
+#comment 
+
+# single line comment
+
+# Multiline comment
+
+# it explain python code
+# python code more readable
+# prevent execution  when testing  code
+
+#statement
+
+print("Hello!");print( "How are you");print("Bye Bye!")
+
+
+#Print Without a New Line
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+print("I am", 35, "years old.")
+
