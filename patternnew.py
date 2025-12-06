836227d495b62dc36f52182a75455205684598f4
Balamurugan <balamuruganelangovan.official@gmail.com>
2025-12-06 23:16:12 +0530

    Pattern new added

--- patternnew.py
+++ patternnew.py
@@ -1,3 +1,10 @@
+# n=int(input("enter the value:"))
+# for i in range(n):
+#     print("*",end="")
+
+
 n=int(input("enter the value:"))
-for i in range(n):
-    print("*",end="")
+for i in range(1,n+1):
+    for i in range(1,i+1):
+        print("*",end="")
+    print()
