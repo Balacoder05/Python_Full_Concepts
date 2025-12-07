5722d51aa2e8f9979aa5b79332d97102ea675b73
Balamurugan <balamuruganelangovan.official@gmail.com>
2025-12-07 23:23:18 +0530

    pattern

--- patternnew.py
+++ patternnew.py
@@ -8,3 +8,9 @@ for i in range(1,n+1):
     for i in range(1,i+1):
         print("*",end="")
     print()
+
+n=int(input("enter the value:"))
+for i in range(1,n+1):
+    for i in range(1,i+1):
+        print("*",end="")
+    print()
